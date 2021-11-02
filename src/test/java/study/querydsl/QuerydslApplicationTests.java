938a0e5f88353c06bb082e56d197807e11b230b3
mac <dudrjsxo3@gmail.com>
2021-11-02 21:10:06 +0900

    lecture 5 end

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -13,6 +14,7 @@ import javax.persistence.PersistenceContext;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
     @PersistenceContext
@@ -25,7 +27,7 @@ class QuerydslApplicationTests {
         em.persist(hello);
 
         JPAQueryFactory query = new JPAQueryFactory(em);
-        QHello qHello = new QHello("h");
+        QHello qHello = QHello.hello;
 
         Hello result = query
                 .selectFrom(qHello)
